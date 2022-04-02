03edee1652ef2f2d442d9f5770750463339a2feb
louisslater <louis.edward.slater@gmail.com>
2022-04-02 18:48:04 +0100

    brambles

---
+++ Assets/Scripts/brambles.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class brambles : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D hitInfo)
+    {
+        //Damage player when player collides with spikes
+        PlayerHealth player = hitInfo.GetComponent<PlayerHealth>();
+        if (player != null)
+        {
+            player.DamagePlayer(5);
+        }
+    }
+}
